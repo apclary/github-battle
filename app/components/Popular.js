f5e43681cec19d2f9f256f68778758c72482e8ba
Alexander Clary <apclary@gmail.com>
2017-04-17 19:10:23 -0500

    Fixed menu not getting selected properly

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,35 +1,39 @@
 var React = require('react');
 
 class Popular extends React.Component {
-  constructor (props) {
-    super(props);
+  constructor(props) {
+    super();
     this.state = {
-      selectedLanguage: 'All'
+      selectedLanguage: 'All',
     };
+
     this.updateLanguage = this.updateLanguage.bind(this);
   }
   updateLanguage(lang) {
-    this.setState(function(lang) {
+    this.setState(function () {
       return {
-        selectedLanguage: lang
+        selectedLanguage: lang,
       }
     });
   }
   render() {
     var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
+
     return (
-      <ul className='languages'>
-        {languages.map(function (lang) {
-          return (
-            <li
-              style={lang == this.state.selectedLanguage ? { color: '#d0021b'} : null}
-              onClick={this.updateLanguage.bind(null, lang)}
-              key={lang}>
-              {lang}
-            </li>
-          );
-        }, this)}
-      </ul>
+      <div>
+        <ul className='languages'>
+          {languages.map(function (lang) {
+            return (
+              <li
+                style={lang === this.state.selectedLanguage ? {color: '#d0021b'} : null}
+                onClick={this.updateLanguage.bind(null, lang)}
+                key={lang}>
+                  {lang}
+              </li>
+            )
+          }, this)}
+        </ul>
+      </div>
     )
   }
 }
